ca026426d35e1708796bdff05a699d504c7095f9
laurarenders <77204794+laurarenders@users.noreply.github.com>
2022-03-02 18:02:57 +0000

    Delete persistentie directory

--- persistentie/Connectie.java
+++
@@ -1,7 +0,0 @@
-package persistentie;
-
-public class Connectie {
-	//Connectie met databank
-	public static final String JBDC_URL = "jdbc:mysql://ID343683_g7.db.webhosting.be:3306/ID343683_g7?serverTimezone=UTC&useLegacyDatetimeCode=false&user=ID343683_g7&password=FBBDEYLR-g07";
-
-}
